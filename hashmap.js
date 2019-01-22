7aee47436d8f2f6edbea56d9de7512ad5ddf2095
Scott Ian <sim2487@gmail.com>
2019-01-22 09:05:56 -0800

    first commit

---
+++ hashmap.js
@@ -0,0 +1,118 @@
+class HashMap {
+  constructor(initialCapacity=8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._slots[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+
+    const index = this._findSlot(key);
+    console.log(index);
+    this._slots[index] = {
+
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
+  }
+
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i=start; i<start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key == key && !slot.deleted)) {
+        return index;
+      }
+    }
+  }
+
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length -
+    //it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i=0; i<string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
+}
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
+
+const lotr = new HashMap;
+
+// console.log(lotr.set("Hobbit", "Bilbo"));
+// console.log(lotr);
+// const arr = [
+//   {Hobbit:"Bilbo"},
+//   {Hobbit:"Frodo"},
+//   {Wizard:"Gandolf"},
+//   {Human:"Aragon"},
+//   {Elf: "Legolas"},
+//   {Maiar:"The Necromancer"},
+//   {Maiar: "Sauron"},
+//   {RingBearer: "Gollum"},
+//   {LadyOfLight: "Galadriel"},
+//   {HalfElven: "Arwen"},
+//   {Ent: "Treebeard"}
+// ];
+
+// lotr.set('Hobbit', 'Bilbo');
+// lotr.set('Hobbit', 'Frodo');
+// lotr.set('Wizard', 'Gandolf');
+// lotr.set('Human', 'Aragon');
+lotr.set('Maiar', 'The Necromancer');
+lotr.set('Maiar', 'Sauron');
+// lotr.set('RingBearer', 'Gollum');
+// lotr.set('LadyOfLight', 'Galadriel');
+// lotr.set('HalfElven', 'Arwen');
+// lotr.set('Ent', 'Treebeard');
+
+//console.log(arr.map(obj => lotr.set(Object.keys(obj).toString(), Object.values(obj).toString())));
+//console.log(lotr);
+//console.log(lotr.get("Maiar"));
